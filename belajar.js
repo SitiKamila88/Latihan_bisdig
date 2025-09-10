822fa810034e04806821e76d974e457098c362ba
SitiKamila88 <ozzamue@gmail.com>
2025-09-10 15:12:09 +0700

    belajar.js

---
+++ belajar.js
@@ -0,0 +1,10 @@
+window.onload = function() {
+    alert('selamat datang di website akyuuh');
+}
+confirm("apakah anda ingin melanjutkan?");
+prompt("saya belajar");
+Date(10)
+Math.random()
+Math.round(4.6)
+parseInt("123")
+setTimeout(function() {alert("selamat datang di website kamila");}, 3000);
